d620ac9f4a78fcb65600859177214732ade25f09
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-06-21 14:41:08 +0800

    Added new file for testing the tab component

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,33 +1,11 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { duotoneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Badges01, { codeSnippet } from './application-ui/elements/badges/01/Badges01';
 
 const Tab = () => {
   const [activeTab, setActiveTab] = useState('preview');
-  const codeSnippets = `
-    <div class="bg-white py-24 sm:py-32">
-      <div class="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
-        <div class="max-w-2xl">
-          <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet our leadership</h2>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Libero fames augue nisl porttitor nisi, quis. Id ac elit odio vitae elementum enim vitae ullamcorper suspendisse.</p>
-        </div>
-        <ul role="list" class="grid gap-x-8 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
-          <li>
-            <div class="flex items-center gap-x-6">
-              <img class="h-16 w-16 rounded-full" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-              <div>
-                <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">Leslie Alexander</h3>
-                <p class="text-sm font-semibold leading-6 text-indigo-600">Co-Founder / CEO</p>
-              </div>
-            </div>
-          </li>
-
-          <!-- More people... -->
-        </ul>
-      </div>
-    </div>
-  `;
 
   return (
     <>
@@ -62,7 +40,7 @@ const Tab = () => {
               transition={{ duration: 0.3 }}
               className='border p-4'
             >
-              <p>This is where the preview content will be displayed.</p>
+              <Badges01 />
             </motion.div>
           ) : (
             <motion.div
@@ -73,8 +51,8 @@ const Tab = () => {
               transition={{ duration: 0.3 }}
               className='border'
             >
-              <SyntaxHighlighter language="html" style={duotoneDark} className='rounded-lg'>
-                {codeSnippets}
+              <SyntaxHighlighter language="html" style={oneDark} className='rounded-lg'>
+                {codeSnippet}
               </SyntaxHighlighter>
             </motion.div>
           )}
---
+++ src/components/application-ui/elements/badges/01/Badges01.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const codeSnippet = `<span class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Badge</span>
+<span class="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Badge</span>
+<span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-indigo-50 px-2 py-1 text-xs font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-purple-50 px-2 py-1 text-xs font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Badge</span>
+<span class="inline-flex items-center rounded-md bg-pink-50 px-2 py-1 text-xs font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">Badge</span>
+`;
+const Badges01 = () => {
+  return (
+    <div className='space-x-2'>
+      <span className="inline-flex items-center rounded-md bg-gray-50 px-4 py-2.5 text-sm font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10">Badge</span>
+      <span className="inline-flex items-center rounded-md bg-red-50 px-4 py-2.5 text-sm font-medium text-red-700 ring-1 ring-inset ring-red-600/10">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-yellow-50 px-4 py-2.5 text-sm font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-green-50 px-4 py-2.5 text-sm font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-blue-50 px-4 py-2.5 text-sm font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-indigo-50 px-4 py-2.5 text-sm font-medium text-indigo-700 ring-1 ring-inset ring-indigo-700/10">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-purple-50 px-4 py-2.5 text-sm font-medium text-purple-700 ring-1 ring-inset ring-purple-700/10">Badge</span>
+      <span class="inline-flex items-center rounded-md bg-pink-50 px-4 py-2.5 text-sm font-medium text-pink-700 ring-1 ring-inset ring-pink-700/10">Badge</span>
+    </div>
+  );
+};
+
+export default Badges01;
