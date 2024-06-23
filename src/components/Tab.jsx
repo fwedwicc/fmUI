f944e0aa00e5fc47ebd7f7f0ff4ec616b8a18c18
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-06-23 22:06:08 +0800

    Installed react icons, and also added custom palettes for exploration

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -2,29 +2,30 @@ import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { oneDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { RxCode, RxEyeOpen } from "react-icons/rx";
 
 const Tab = ({ previewContent, codeSnippet }) => {
   const [activeTab, setActiveTab] = useState('preview');
 
   return (
     <>
-      <div className='inline-flex p-1.5 border gap-2 rounded-[0.4rem]'>
+      <div className='inline-flex p-1.5 border border-neutral-400/40 gap-2 rounded-[0.4rem]'>
         <motion.button
-          className={`text-center px-4 py-2 rounded-md ${activeTab === 'preview' ? 'text-indigo-500 bg-indigo-500/10 font-medium' : 'text-gray-500'
+          className={`inline-flex justify-center items-center gap-2.5 px-4 py-2 rounded-md ${activeTab === 'preview' ? 'text-zinc-200 bg-neutral-700 font-medium' : 'text-zinc-600 bg-neutral-300 hover:bg-neutral-400/50 transition duration-300 ease-in-out'
             }`}
           onClick={() => setActiveTab('preview')}
-          whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
+          <RxEyeOpen />
           Preview
         </motion.button>
         <motion.button
-          className={`text-center px-4 py-2 rounded-md ${activeTab === 'code' ? 'text-indigo-500 bg-indigo-500/10 font-medium' : 'text-gray-500'
+          className={`inline-flex justify-center items-center gap-2.5 px-4 py-2 rounded-md ${activeTab === 'code' ? 'text-zinc-200 bg-neutral-700 font-medium' : 'text-zinc-600 bg-neutral-300 hover:bg-neutral-400/50 transition duration-300 ease-in-out'
             }`}
           onClick={() => setActiveTab('code')}
-          whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
         >
+          <RxCode />
           Code
         </motion.button>
       </div>
