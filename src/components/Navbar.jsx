4de598a765158e4543b96f7fa5703e0eea2a5d95
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-06-29 14:33:01 +0800

    Push/commit for updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,9 @@ const Navbar = () => {
 
   return (
     <div className='fixed flex w-full h-[4.5rem] justify-between items-center px-[4rem] backdrop-blur-md bg-neutral-50/20 z-50'>
-      <img src={fmUILogo} alt="fmUI Logo" className='w-auto h-7' />
+      <Link to='/home'>
+        <img src={fmUILogo} alt="fmUI Logo" className='w-auto h-7' />
+      </Link>
       <div className='flex gap-1.5 border p-1.5 rounded-lg items-center'>
         <Link className={linkClasses('/home')} to='/home'>
           <HiHome />
--- src/components/application-ui/elements/buttons/Buttons09.jsx
+++ src/components/application-ui/elements/buttons/Buttons09.jsx
@@ -22,26 +22,26 @@ const Buttons09 = () => {
   return (
     <div className='gap-3 flex justify-center items-center flex-wrap p-6'>
       {/* Orange to Red */}
-      <button class="group inline-block rounded-md bg-gradient-to-r from-orange-400 to-red-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
-        <span class="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
+      <button className="group inline-block rounded-md bg-gradient-to-r from-orange-400 to-red-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
+        <span className="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
           Orange to red
         </span>
       </button>
       {/* Green to sky */}
-      <button class="group inline-block rounded-md bg-gradient-to-r from-green-400 to-sky-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
-        <span class="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
+      <button className="group inline-block rounded-md bg-gradient-to-r from-green-400 to-sky-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
+        <span className="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
           Green to sky
         </span>
       </button>
       {/* Indigo to Purple */}
-      <button class="group inline-block rounded-md bg-gradient-to-r from-indigo-400 to-purple-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
-        <span class="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
+      <button className="group inline-block rounded-md bg-gradient-to-r from-indigo-400 to-purple-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
+        <span className="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
           Indigo to purple
         </span>
       </button>
       {/* Rose to Pink */}
-      <button class="group inline-block rounded-md bg-gradient-to-r from-pink-400 to-rose-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
-        <span class="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
+      <button className="group inline-block rounded-md bg-gradient-to-r from-pink-400 to-rose-500 p-[2px] transition-colors duration-[5ms] ease-in-out hover:text-white">
+        <span className="block rounded bg-white px-4 py-2.5 group-hover:font-semibold transition-colors duration-300 ease-in-out group-hover:bg-transparent">
           Rose to pink
         </span>
       </button>
