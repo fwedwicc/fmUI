7d8b6ee3e9816dc7f7f624309f2bdde3493cde62
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-02 10:18:08 +0800

    Just added one last variant of alert component

--- src/components/application-ui/elements/alerts/Alert04.jsx
+++ src/components/application-ui/elements/alerts/Alert04.jsx
@@ -1,13 +1,63 @@
 import React from 'react';
 
-export const codeSnippet = `TODO:
+export const codeSnippet = `<div class="inline-block rounded-lg border border-neutral-200 bg-neutral-100 p-4 text-sm text-zinc-800" role="alert">
+  <div class="flex justify-between">
+    <div class="flex items-center">
+      <svg class="me-3 inline h-5 w-5 flex-shrink-0" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clip-rule="evenodd" />
+      </svg>
+      <h2 class="text-[1.1rem]">Default alert</h2>
+    </div>
+    <button class="flex h-8 w-8 items-center justify-center rounded-md transition duration-300 ease-in-out hover:bg-neutral-200/40 focus:ring-2 focus:ring-neutral-200">
+      <svg class="h-auto w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path fill-rule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+  <div class="mt-2.5"><span class="font-medium">Default alert!</span> Some alert message goes here.</div>
+  <div class="mt-4 flex gap-2">
+    <button class="inline-flex items-center justify-center gap-1.5 rounded-md border border-neutral-900 bg-neutral-700 px-2 py-1 font-medium text-zinc-100 transition duration-300 ease-in-out hover:bg-neutral-900/90 focus:border-none focus:ring-2 focus:ring-neutral-400">
+      Review
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4">
+        <path fill-rule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clip-rule="evenodd" />
+      </svg>
+    </button>
+    <button class="rounded-md border border-neutral-300 bg-neutral-200 px-2 py-1 font-medium text-zinc-600 transition duration-300 ease-in-out hover:bg-neutral-400/40 focus:border-none focus:ring-2 focus:ring-neutral-400/70">Revert</button>
+  </div>
+</div>
 `;
 
 const Alert04 = () => {
   return (
     <div className='gap-3 flex justify-center items-center flex-wrap p-6'>
       <div className='flex flex-col items-start gap-3'>
-        helo
+        <div className="inline-block p-4 text-sm text-zinc-800 rounded-lg bg-neutral-100 border border-neutral-200" role="alert">
+          <div className='flex justify-between'>
+            <div className='flex items-center'>
+              <svg className="flex-shrink-0 inline w-5 h-5 me-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd" />
+              </svg>
+              <h2 className='text-[1.1rem]'>Default alert</h2>
+            </div>
+            <button className='w-8 h-8 flex justify-center items-center transition duration-300 ease-in-out hover:bg-neutral-200/40 rounded-md focus:ring-2 focus:ring-neutral-200'>
+              <svg className='w-4 h-auto' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+              </svg>
+            </button>
+          </div>
+          <div className='mt-2.5'>
+            <span className="font-medium">Default alert!</span> Some alert message goes here.
+          </div>
+          <div className='mt-4 flex gap-2'>
+            <button className='px-2 py-1 inline-flex justify-center items-center gap-1.5 text-zinc-100 hover:bg-neutral-900/90 transition duration-300 ease-in-out bg-neutral-700 border border-neutral-900 font-medium rounded-md focus:border-none focus:ring-2 focus:ring-neutral-400'>
+              Review
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-4">
+                <path fillRule="evenodd" d="M15.75 2.25H21a.75.75 0 0 1 .75.75v5.25a.75.75 0 0 1-1.5 0V4.81L8.03 17.03a.75.75 0 0 1-1.06-1.06L19.19 3.75h-3.44a.75.75 0 0 1 0-1.5Zm-10.5 4.5a1.5 1.5 0 0 0-1.5 1.5v10.5a1.5 1.5 0 0 0 1.5 1.5h10.5a1.5 1.5 0 0 0 1.5-1.5V10.5a.75.75 0 0 1 1.5 0v8.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V8.25a3 3 0 0 1 3-3h8.25a.75.75 0 0 1 0 1.5H5.25Z" clipRule="evenodd" />
+              </svg>
+            </button>
+            <button className='px-2 py-1 transition duration-300 ease-in-out text-zinc-600 bg-neutral-200 border border-neutral-300 hover:bg-neutral-400/40 font-medium rounded-md focus:border-none focus:ring-2 focus:ring-neutral-400/70'>Revert</button>
+          </div>
+        </div>
       </div>
     </div>
   );
