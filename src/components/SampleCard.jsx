f0e98aa3ae230fb28ef66123d4d1c5f006d9554d
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-07-14 13:43:00 +0800

    Just installed jdion tilt effect library and tested it in one sample component

--- src/components/SampleCard.jsx
+++ src/components/SampleCard.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import useVanillaTilt from '../hooks/useVanillaTilt';
+import { Tilt } from '@jdion/tilt-react';
 
 const SampleCard = () => {
-  const tiltOptions = {
-    max: 7,
-    speed: 50,
-    glare: true,
-    "max-glare": 0.3,
-  };
-  const cardRef = useVanillaTilt(tiltOptions);
 
   const imageUrls = [
     'https://placehold.co/30x30',
@@ -39,46 +32,52 @@ const SampleCard = () => {
   );
 
   return (
-    <div ref={cardRef} className='p-4 rounded-md border border-neutral-400/30 w-full h-full space-y-5 shadow-xl'>
-      <div>
-        <img src="https://placehold.co/600x300" alt="" className='w-full h-full rounded-md object-cover' />
-      </div>
-      <div className='space-y-4 px-4 pt-4'>
-        <div className='flex gap-1.5 flex-wrap'>
-          {tags.map((tag, index) => (
-            <span key={index} className="whitespace-nowrap rounded-full border border-neutral-300 bg-neutral-100 px-2.5 py-1.5 text-sm text-zinc-800">
-              {tag}
-            </span>
-          ))}
+    <Tilt
+      className="Tilt"
+      options={{ max: 15, scale: 1.02, speed: 400 }}
+      style={{ height: '100%', width: '100%' }}
+    >
+      <div className='p-4 rounded-md border border-neutral-400/30 w-full h-full space-y-5 shadow-xl'>
+        <div>
+          <img src="https://placehold.co/600x300" alt="" className='w-full h-full rounded-md object-cover' />
         </div>
-        <div className='space-y-2'>
-          <div className='flex justify-between items-center'>
-            <h1 className='text-[1.5rem]'>Card Title</h1>
-            <h1 className='text-[1.5rem]'>$1099.00</h1>
-          </div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus aperiam sint rerum accusantium.</p>
-          <div className='flex gap-4 items-center pt-2'>
-            <div className='flex'>
-              {imageUrls.map((url, index) => (
-                <img
-                  key={index}
-                  src={url}
-                  alt={`Image ${index + 1}`}
-                  className={`${index !== 0 ? 'ml-[-0.4rem]' : ''} rounded-full ring-1 ring-white`}
-                />
-              ))}
-            </div>
-            <span>Join 867+ members</span>
-          </div>
-          <div className="flex items-center pt-3">
-            {stars.map((star, index) => (
-              <StarIcon key={index} color={star.color} />
+        <div className='space-y-4 px-4 pt-4'>
+          <div className='flex gap-1.5 flex-wrap'>
+            {tags.map((tag, index) => (
+              <span key={index} className="whitespace-nowrap rounded-full border border-neutral-300 bg-neutral-100 px-2.5 py-1.5 text-sm text-zinc-800">
+                {tag}
+              </span>
             ))}
-            <span className='ms-1 text-sm font-medium text-neutral-700'>4.95 out of 5</span>
+          </div>
+          <div className='space-y-2'>
+            <div className='flex justify-between items-center'>
+              <h1 className='text-[1.5rem]'>Card Title</h1>
+              <h1 className='text-[1.5rem]'>$1099.00</h1>
+            </div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus aperiam sint rerum accusantium.</p>
+            <div className='flex gap-4 items-center pt-2'>
+              <div className='flex'>
+                {imageUrls.map((url, index) => (
+                  <img
+                    key={index}
+                    src={url}
+                    alt={`Image ${index + 1}`}
+                    className={`${index !== 0 ? 'ml-[-0.4rem]' : ''} rounded-full ring-1 ring-white`}
+                  />
+                ))}
+              </div>
+              <span>Join 867+ members</span>
+            </div>
+            <div className="flex items-center pt-3">
+              {stars.map((star, index) => (
+                <StarIcon key={index} color={star.color} />
+              ))}
+              <span className='ms-1 text-sm font-medium text-neutral-700'>4.95 out of 5</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Tilt>
   )
 }
 
