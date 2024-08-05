55eaf0b76d60402cdb45488b1b4c4fe6fabba276
fwedwicc <moreno.frederick.capiral@gmail.com>
2024-08-05 13:18:11 +0800

    A bit modification in routing for fixing

--- src/main.jsx
+++ src/main.jsx
@@ -7,29 +7,34 @@ import Home from './pages/Home';
 import Components from './pages/Components';
 import ComponentOverview from './pages/ComponentOverview';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/fmUI/",
+      element: <Home />,
+      children: [
+        {
+          path: "/fmUI/home",
+          element: <Home />,
+        },
+        {
+          path: "/fmUI/components",
+          element: <Components />,
+        },
+        {
+          path: "/fmUI/components/:type",
+          element: <ComponentOverview />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/fmUI/",
-    element: <App />,
-    children: [
-      {
-        path: "home",
-        element: <Home />,
-      },
-      {
-        path: "components",
-        element: <Components />,
-      },
-      {
-        path: "components/:type",
-        element: <ComponentOverview />,
-      },
-    ],
-  },
-]);
+    basename: import.meta.env.BASE_URL,
+  }
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
